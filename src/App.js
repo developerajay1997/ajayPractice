8adad262953f06086a8c57cc3d719716bb797e87
daman kumar <damannetweb@gmail.com>
2022-07-15 17:16:13 +0530

    ajay

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Footer from './Footer';
+
+function App() {
+
+  return (
+    <div className='App ' style={{}}>
+      <Footer />
+      <Header nme = "Ajay"/>
+      <Header nme = "Rahul"/>
+      <Header nme = "Savraj"/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,11 @@
+
+function Header(p) {
+    function Apple(){
+        
+    }
+    return (
+      <h1>hiii {p.nme}</h1>
+    );
+  }
+  
+  export default Header;
